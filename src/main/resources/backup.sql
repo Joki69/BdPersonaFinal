2c5ad26c571fe5579f80b42b9c3c88af74a70ed2
Joki69 <Jcarralero@elpuig.xeill.net>
2023-03-07 19:34:53 +0100

    Funcional 100%

--- src/main/resources/backup.sql
+++ src/main/resources/backup.sql
@@ -4,15 +4,24 @@ nombre varchar(1000),
 PRIMARY KEY(id_arcana));
 
 
+CREATE TABLE debilidades (
+id_debilidad serial,
+nombre_debilidad varchar(5000),
+PRIMARY KEY(id_debilidad));
+
 CREATE TABLE persona (
 id_persona serial,
 id_arcana integer,
+id_debilidad integer,
 nombre_arcana varchar(1000),
 nombre_persona text,
 historia text,
 PRIMARY KEY(id_persona),
    CONSTRAINT fk_arcana
       FOREIGN KEY(id_arcana) 
-	  REFERENCES arcana(id_arcana)
+	  REFERENCES arcana(id_arcana),
+   CONSTRAINT fk_debilidad
+      FOREIGN KEY(id_debilidad)
+      REFERENCES debilidades(id_debilidad)
 );
 
